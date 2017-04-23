2c2de90a9c6328a8bbb7df23f2d420fc1a68630a
sandiprb <sandip@coverfoxmail.com>
2017-04-23 19:56:09 +0530

    More views & template added

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tagging',
+    'cfblog',
 ]
 
 MIDDLEWARE_CLASSES = [
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils.functional import SimpleLazyObject
+
+from .models import Question
 
 # Create your views here.
 def index(request):
-	return HttpResponse('Hello World!')
+	latest_question_list = Question.objects.all().values()
+	print (latest_question_list)
+	template = 'polls/index.html'
+	context = {
+		'latest_question_list': latest_question_list,
+	}
+	return render(request, template, context)
+
+
+def detail(request, question_id):
+	print(request)
+	return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+	response = "You're looking at the results of question %s."
+	return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+	return HttpResponse("You're voting on question %s." % question_id)
